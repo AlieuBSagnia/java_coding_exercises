5d1e19d2c3048401ba10fa0124d996ce426e4767
AlieuBSagnia <alieubsagnia@gmail.com>
2021-03-13 16:52:16 +0000

    Exercise 1 addVat function complete

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,8 +1,11 @@
 package com.techreturners.exercise001;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class Exercise001 {
+
     public String capitalizeWord(String word) {
         
         return Character.toUpperCase(word.charAt(0)) + word.substring(1);
@@ -14,8 +17,13 @@ public class Exercise001 {
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        
+        double vatElement = originalPrice * vatRate / 100;
+        double priceWithVAT = originalPrice + vatElement;
+
+        BigDecimal bd = new BigDecimal(priceWithVAT).setScale(2, RoundingMode.HALF_UP);
+        double salary = bd.doubleValue();
+        return salary;
     }
 
     public String reverse(String sentence) {
@@ -32,6 +40,7 @@ public class Exercise001 {
     }
 
     public int countLinuxUsers(List<User> users) {
+        
         int count = 0;
 
         for(int i = 0; i < users.size(); i++){
